1dde0eac072525783a004d5797f01749849442bc
vaishnavisuryawanshi102 <vaishnavisuryawanshi102@gmail.com>
2026-07-21 19:52:06 +0530

    string.js

---
+++ string.js
@@ -0,0 +1,110 @@
+name1 = 'Vaishnavi'
+name2 = "Suryawanshi"
+name = name1 + name2
+console.log(name)
+
+str = 'I Like to learn javascript'
+console.log(name.length)
+console.log(str.length)
+
+console.log(name.charAt(0))
+console.log(name[0])
+
+//toUppercase and toLowercase
+
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+
+
+//.concat()
+
+str1 = "Hello"
+str2 = "World"
+msg = str1.concat(" ", str2, "!!!" )
+console.log(msg)
+
+//.indexof()
+
+indexNumber = name.indexOf("Vaishnavi")
+console.log(indexNumber)
+
+index1 = msg.indexOf("hello")
+console.log(index1)            //return -1 because not exists
+index2 = msg.indexOf("Hello")
+console.log(index1)
+
+
+console.log(msg.indexOf('o'))
+console.log(msg.lastIndexOf('o'))
+
+
+//return boolean value true or false
+console.log(msg.includes("Hello"))
+console.log(msg.includes("iuytre"))
+
+console.log(msg.startsWith("Hello"))
+console.log(msg.endsWith("hello"))
+
+console.log(msg.endsWith("!!"))
+console.log(msg.endsWith("World!!!"))
+
+//first parameter index and second parameter is also index
+substr = msg .slice(0, 4)
+console.log
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// .trim(), .trimStart(), .trimEnd()
+
+msg1 = "                  Hello                       "
+console.log(msg1.length)
+console.log(msg1.trim().length)
+console.log(msg1.trimStart().length)
+console.log(msg1.trimEnd().length)
+
+
+//  .replace() and .replaceAll().
+str10 = str.replace('like', 'love')
+console.log(str10)
+console.log(str)
+msg3 = "It's okay is you dont like computer programming. It's smart people thing anyway"
+msg4 =  msg3.replaceAll("It's", "Its")
+console.log(msg4)
+
+//.split()
+
+fruitsString = "Apple Banana Mango"
+fruitArray = fruitsString.split('')
+console.log(fruitArray)
+console.log(typeof(fruitArray))
+
+
+
+
+
+
+
+
+
+
